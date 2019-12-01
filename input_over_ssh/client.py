63ed05387e6f18ac2ff4c248ab2ef58403c1cf28
RunasSudo <runassudo@yingtongli.me>
2019-12-01 20:52:55 +1100

    Initial commit of evdev-based version

---
+++ input_over_ssh/client.py
@@ -0,0 +1,76 @@
+#   input-over-ssh: Mouse and keyboard forwarding over SSH
+#   Copyright © 2019  Lee Yingtong Li (RunasSudo)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+PROTOCOL_VERSION = '1'
+
+import argparse
+import asyncio
+import evdev
+import json
+
+async def forward_device(i, device):
+	async for event in device.async_read_loop():
+		print(json.dumps([i, event.type, event.code, event.value]))
+
+def encode_device(device):
+	cap = device.capabilities()
+	del cap[0] # Filter out EV_SYN, otherwise we get OSError 22 Invalid argument
+	cap_json = {}
+	for k, v in cap.items():
+		cap_json[k] = [x if not isinstance(x, tuple) else [x[0], x[1]._asdict()] for x in v]
+	return {'name': device.name, 'capabilities': cap_json}
+
+async def run_forward():
+	# Find devices
+	devices_by_name = {}
+	if args.device_by_name:
+		for path in evdev.list_devices():
+			device = evdev.InputDevice(path)
+			devices_by_name[device.name] = device
+	
+	devices = []
+	for path in args.device_by_path:
+		devices.append(evdev.InputDevice(path))
+	for name in args.device_by_name:
+		devices.append(devices_by_name[name])
+	
+	# Report version
+	print(PROTOCOL_VERSION)
+	
+	# Report devices
+	print(json.dumps([encode_device(device) for device in devices]))
+	
+	tasks = []
+	for i, device in enumerate(devices):
+		tasks.append(asyncio.create_task(forward_device(i, device)))
+	
+	await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+async def list_devices():
+	for path in evdev.list_devices():
+		device = evdev.InputDevice(path)
+		print('{}  {}'.format(device.path, device.name))
+
+parser = argparse.ArgumentParser(description='input-over-ssh client')
+parser.add_argument('-L', '--list-devices', dest='action', action='store_const', const=list_devices, help='List available input devices')
+parser.add_argument('-p', '--device-by-path', action='append', default=[], help='Forward device with the given path')
+parser.add_argument('-n', '--device-by-name', action='append', default=[], help='Forward device with the given name')
+args = parser.parse_args()
+
+if not args.action:
+	args.action = run_forward
+
+asyncio.run(args.action())
---
+++ input_over_ssh/server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#   input-over-ssh: Mouse and keyboard forwarding over SSH
+#   Copyright © 2019  Lee Yingtong Li (RunasSudo)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+PROTOCOL_VERSION = '1'
+
+import evdev
+import json
+
+version = input()
+if version != PROTOCOL_VERSION:
+	raise Exception('Invalid protocol version. Got {}, expected {}.'.format(version, PROTOCOL_VERSION))
+
+devices_json = json.loads(input())
+devices = []
+for device_json in devices_json:
+	capabilities = {}
+	for k, v in device_json['capabilities'].items():
+		capabilities[int(k)] = [x if not isinstance(x, list) else (x[0], evdev.AbsInfo(**x[1])) for x in v]
+	devices.append(evdev.UInput(capabilities, name=device_json['name'] + ' (via input-over-ssh)'))
+
+print('Device created')
+
+while True:
+	event = json.loads(input())
+	#print(event)
+	devices[event[0]].write(event[1], event[2], event[3])
